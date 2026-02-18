17e3aa107916e80eb3db463272d57571e6dd9349
lena <lena.odum1@gmail.com>
2026-02-18 10:38:46 +0100

    ex2 done

--- 02_python_piscine/02_M02/ex2/ft_custom_errors.py
+++ 02_python_piscine/02_M02/ex2/ft_custom_errors.py
@@ -1,26 +1,42 @@
+class GardenError(Exception):
+    pass
+class PlantError(GardenError):
+    pass
+class WaterError(GardenError):
+    pass
+
+def water_plant(amount):
+    if amount < 5:
+        raise WaterError("Not enough water in the tank!")
+
+def check_plant(health):
+    if health < 5:
+        raise PlantError("The tomato plant is wilting!")
+
 def raise_error():
+    print("\nTesting WaterError...")
     try:
-        pass
-    except:
-        print("Caught PlantError:")
-        
+        water_plant(2)
+    except WaterError as e:
+        print(f"Caught WaterError: {e}")
+    print("\nTesting PlantError")
     try:
-        pass
-    except:
-        print("Caught WaterError")
+       check_plant(2)
+    except PlantError as e:
+        print(f"Caught PlantError: {e}")
+    print("\nTesting catching all garden errors...")
+    try:
+        check_plant(2)
+    except GardenError as e:
+        print(f"Caught a garden error: {e}")
+    try:
+        water_plant(2)
+    except GardenError as e:
+        print(f"Caught a garden error: {e}")
     
-    try:
-        pass
-    except:
-        print("Caught a garden error:")
     
-    All custom error types work correctly
-
-def print_error():
-    pass
-
-def show_error():
-    pass
+    print("\nAll custom error types work correctly")
 
 if __name__ == "__main__":
     print("=== Custom Garden Errors Demo ===")
+    raise_error()
