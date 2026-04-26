749e4c0adc5cb79469d6150918d9b80bb5cf04b0
lena <lena.odum1@gmail.com>
2026-04-26 22:36:30 +0200

    starting M10:

---
+++ 02_python_piscine_git/10.M10/ex1/lamba_spells.py
@@ -0,0 +1,16 @@
+def artifact_sorter(artifacts: list[dict]) -> list[dict]:
+    return sorted(artifacts, key=lambda artifact: artifact["power"], reverse=True)  
+
+def power_filter(mages: list[dict], min_power: int) -> list[dict]:
+    return list(filter(lambda m: m["power"] >= min_power, mages)
+     
+
+def spell_transformer(spells: list[str]) -> list[str]:
+    return list(map(lambda x, "* " + x + " *", spells))
+
+def mage_stats(mages: list[dict]) -> dict:
+    return {
+        'max_power': max(mages, key=lambda m: m['power'])['power'],
+        'min_power': min(mages, key=lambda m: m['power'])['power'],
+        'avg_power': round(sum(m['power'] for m in mages) / len(mages), 2),
+        }
